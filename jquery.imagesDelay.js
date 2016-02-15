d16a749b252eecd32531975b1561bf4bbb0bf070
Riny <rmengfei@foxmail.com>
2016-02-15 19:18:45 +0800

    代码优化

--- jquery.imagesDelay.js
+++ jquery.imagesDelay.js
@@ -3,18 +3,19 @@
 * PluginName: imagesDelay
 */
 ;(function($) {
+	// 可以不用 extend 方法
 	$.fn.imagesDelay = function(distance) {
 
-		// 获取所有img对象
+		// 获取所有 img 对象
 		var $images = $(this);
 
 		// 获取视口的高度
 		var $H = $(window).height();
 
-		// 触发距离,默认距100px时触发加载
+		// 触发距离,默认距 100px 时触发加载
 		var $D = 100;
 
-		// 如果有传参数,且参数为number
+		// 如果有传参数,且参数为 number
 		if (typeof(distance) == 'number') {
 			 $D = distance;
 		}
@@ -25,19 +26,18 @@
 			// 获取滚动条的高度
 			var $S = $(window).scrollTop();
 
-			// 遍历所有的img对象
+			// 遍历所有的 img 对象
 			$.each($images, function(index, val) {
 
-				// 把DOM节点对象 转换 成jQuery对象
+				// 把 DOM 节点对象转换 成 jQuery 对象
 				var $val = $(val);
 
-				//  过滤所有img中不包含data-src属性
+				// 过滤所有 img 中不包含 data-src 属性
 				if(!($val.attr('data-src'))) {
 					return ;
 				}
-				/*当页面底部将要滚动到img的上方时
-				把data-src的值添加到src属性上
-				并删除data-src属性*/
+				/* 当页面底部将要滚动到 img 的上方时,
+				   把data-src的值添加到 src 属性上, 并删除data-src属性 */
 				if ($H + $S + $D > $val.offset().top) {
 					$val.attr('src', $val.attr('data-src'));
 					$val.removeAttr('data-src');
